96aea91f050bf83960e8e0b9aa74fc5ce94e7166
nishitkumawat <nishit1060@gmail.com>
2026-04-14 12:11:01 +0530

    vercel config

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
     };
 
     try {
-      const response = await fetch("https://api.ezrun.in/iot/sgi/", {
+      const response = await fetch("http://127.0.0.1:8000/iot/sgi/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
